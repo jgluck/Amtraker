87d07b0b00d7ba25dd793573c1922eda5cfad559
Jonathan Gluck <jonathangluck08854@gmail.com>
2015-07-20 11:23:05 -0400

    pushing some initial unicorn stuff

---
+++ config.ru
@@ -0,0 +1,9 @@
+require "rubygems"
+require "sinatra"
+require "slim"
+
+
+
+require File.expand_path '../main.rb', __FILE__
+
+run Amtrack
--- main.rb
+++ main.rb
@@ -1,28 +1,33 @@
-require 'sinatra'
-require 'slim'
-
-url = "http://dixielandsoftware.net/cgi-bin/gettrain.pl?seltrain="
-url2 = "&selyear="
-url3 = "&selmonth="
-url4 = "&selday="
+# require 'sinatra'
+# require 'slim'
+# require 'unicorn'
 
 
-get '/' do	
-	'Train Spotting'
-end
+class Amtrack < Sinatra::Base
 
-get '/:trainno/?:year?/?:month?/?:day?' do
-	time = Time.new
-	train = params[:trainno]
-	year = params[:year]
-	month = params[:month]
-	day = params[:day]
-	
+	url = "http://dixielandsoftware.net/cgi-bin/gettrain.pl?seltrain="
+	url2 = "&selyear="
+	url3 = "&selmonth="
+	url4 = "&selday="
 
-	year ||= time.year.to_s
-	month ||= time.strftime("%m")
-	day ||= time.strftime("%d")
 
-	redirect to(url + train + url2 + year + url3 + month + url4 + day)
+	get '/' do	
+		slim :index
+	end
 
+	get '/:trainno/?:year?/?:month?/?:day?' do
+		time = Time.new
+		train = params[:trainno]
+		year = params[:year]
+		month = params[:month]
+		day = params[:day]
+		
+
+		year ||= time.year.to_s
+		month ||= time.strftime("%m")
+		day ||= time.strftime("%d")
+
+		redirect to(url + train + url2 + year + url3 + month + url4 + day)
+
+	end
 end
